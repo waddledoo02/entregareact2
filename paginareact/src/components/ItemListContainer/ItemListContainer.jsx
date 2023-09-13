e19833ce247ed54a81e44a0fd5d7813c2c880a6b
Carlos Hernandez <carloshernandezabreu33@gmail.com>
2023-09-13 11:31:24 -0300

    codigocorregido

--- paginareact/src/components/ItemListContainer/ItemListContainer.jsx
+++ paginareact/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Title from '../Title/Title'
-import ItemCount from '../ItemCount/ItemCount'
+
 const ItemListContainer = (props) => {
   return (
-    <><Title greeting={props.texto}></Title><ItemCount></ItemCount></>
+    <><Title greeting={props.texto}></Title></>
   )
 }
 
