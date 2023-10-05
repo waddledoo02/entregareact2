a1a42e67b052fcaf1eb88ab02204a694412160f5
Carlos Hernandez <carloshernandezabreu33@gmail.com>
2023-10-05 15:18:39 -0300

    firestoreconfigurado

--- paginareact/src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ paginareact/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,23 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import {getFirestore,doc,getDoc} from 'firebase/firestore';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import ItemDetail from '../ItemDetail/ItemDetail';
-import laptopnormal from '../Item/imagenes/laptopnormal.png'
-import telefono from '../Item/imagenes/telefono.jpg'
-import tv from '../Item/imagenes/tv.jpg'
+
 import { useParams } from 'react-router-dom';
 
 const ItemDetailContainer = () => {
-  
+
   const [data, setData] = useState({});
   const { idProducto } = useParams();
 
   useEffect(() => {
-   const querydb = getFirestore();
-   const queryDoc = doc(querydb,'products','cZEvDjvaUuXcZStwSLhu');
-   getDoc(queryDoc)
-   .then(res=>setData   ({id:res.id,...res.data()}))
+    const querydb = getFirestore();
+    const queryDoc = doc(querydb, 'products', idProducto);
+    getDoc(queryDoc)
+      .then(res => setData({ id: res.id, ...res.data() }))
   }, [idProducto]);
-  
+
   return (
     <ItemDetail product={data} />
   )
--- paginareact/src/components/ItemListContainer/ItemListContainer.jsx
+++ paginareact/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,53 +1,26 @@
 import React, { useEffect, useState } from 'react';
-
+import {getFirestore,collection,getDocs,query,where} from 'firebase/firestore';
 import ItemList from '../ItemList/ItemList';
-import laptopnormal from '../Item/imagenes/laptopnormal.png';
-import telefono from '../Item/imagenes/telefono.jpg';
-import tv from '../Item/imagenes/tv.jpg';
 import { useParams } from 'react-router-dom';
 
 const ItemListContainer = (props) => {
-  const products = [
-    {
-      id: 1, image: tv,
-      title: "Televisor",
-      category:"Electrodomesticos",
-      price: 300,
-    },
-    {
-      id: 2, image: telefono,
-      title: "Iphone",
-      category:"Celulares",
-      price : 400,
-    },
-    {
-      id: 3, image: laptopnormal,
-      title: "Laptop",
-      category:"Computadoras",
-      price: 700,
-    }
-  ];
-  console.log(products);
+  
+
 const [data,setData] = useState([]);
 
 const {categoriaId} = useParams();
 
 useEffect(() => {
-  const getData = new Promise(resolve => {
-    setTimeout(() => {
-      resolve(products);
-    }, 3000);
-  });
-
-  if (categoriaId) {
-    getData.then(res => {
-      const filteredData = res.filter(product => product.category === categoriaId);
-      setData(filteredData); 
-      console.log(categoriaId)
-      // Asigna el resultado del filtrado a setData
-    });
-  } else {
-    getData.then(res => setData(res));
+  const querydb = getFirestore();
+  const queryCollection = collection(querydb,'products');
+  if(categoriaId){
+    const queryFilter = query(queryCollection,where('category','===',categoriaId))
+    getDocs(queryFilter)
+    .then(res=> setData(res.docs.map(product=>({id:product.id,...product.data()}))))
+  }
+  else{
+    getDocs(queryCollection)
+    .then(res=> setData(res.docs.map(product=>({id:product.id,...product.data()}))))
   }
 }, [categoriaId]);
 
