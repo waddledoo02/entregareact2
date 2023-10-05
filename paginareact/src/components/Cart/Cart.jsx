30184b67cc38f4574af195516129d1cddeb3ebd9
Carlos Hernandez <carloshernandezabreu33@gmail.com>
2023-10-05 16:51:14 -0300

    proyectofinalreact

--- paginareact/src/components/Cart/Cart.jsx
+++ paginareact/src/components/Cart/Cart.jsx
@@ -1,28 +1,54 @@
-import React from 'react'
-import { useCartContext } from '../../context/CartContext'
+import React, { useState } from 'react';
+import { useCartContext } from '../../context/CartContext';
 import { Link } from 'react-router-dom';
 import ItemCart from '../ItemCart/ItemCart';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 
 const Cart = () => {
-  const {cart,totalPrice}=useCartContext();
-  if(cart.length===0){
-    return(
+  const { cart, totalPrice, clearCart } = useCartContext();
+  const [orderId, setOrderId] = useState(null); // Nuevo estado para el ID de la orden
+
+  const order = {
+    // ...
+  };
+
+  const handleClick = () => {
+    const db = getFirestore();
+    const orderCollection = collection(db, 'orders');
+    addDoc(orderCollection, order)
+      .then(({ id }) => {
+        setOrderId(id); // Actualizar el estado con el ID generado
+      })
+      .catch(error => {
+        console.error('Error al crear la orden:', error);
+      });
+  };
+
+  if (cart.length === 0) {
+    return (
       <>
-      <p>No hay elementos en el carrito</p>
-      <Link to='/'>Hacer Compras</Link>
+        <p>No hay elementos en el carrito</p>
+        <Link to='/'>Hacer Compras</Link>
       </>
-    )
+    );
   }
+
   return (
     <>
-    {
-      cart.map(product=><ItemCart key={product.id} product={product}/>)
-    }
-    <p>
-      total:{totalPrice()}
-    </p>
-    </>
-  )
-}
+      {cart.map(product => <ItemCart key={product.id} product={product} />)}
+      <p>
+        total:{totalPrice()}
+      </p>
+      <button onClick={handleClick}>Emitir compra</button>
+      <button onClick={() => clearCart()}>Limpiar Carrito</button>
 
-export default Cart
+      {orderId && ( // Renderizar el mensaje de confirmación si orderId está presente
+        <div>
+          <p>¡Compra realizada con éxito! ID de la orden: {orderId}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Cart;
--- paginareact/src/components/ItemListContainer/ItemListContainer.jsx
+++ paginareact/src/components/ItemListContainer/ItemListContainer.jsx
@@ -14,7 +14,7 @@ useEffect(() => {
   const querydb = getFirestore();
   const queryCollection = collection(querydb,'products');
   if(categoriaId){
-    const queryFilter = query(queryCollection,where('category','===',categoriaId))
+    const queryFilter = query(queryCollection,where('category','==',categoriaId))
     getDocs(queryFilter)
     .then(res=> setData(res.docs.map(product=>({id:product.id,...product.data()}))))
   }
